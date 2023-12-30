6981e8a613a7a6142f6392fdb4193619625032d2
Ana Beatriz <ana.mello@outlook.com>
2023-12-29 22:10:51 -0300

    functions splitting

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
 let readline = require("readline-sync")
 let tipo
 let ataque
+let classeHeroi
 
 function classe() {
-    let classeHeroi = parseInt(readline.question("\n---- CLASSES DE HEROI ----\n|     1 - Guerreiro      |\n|     2 - Mago           |\n|     3 - Monge          |\n|     4 - Ninja          |\n--------------------------\nEscolha a classe do seu heroi: "))
+    classeHeroi = parseInt(readline.question("\n---- CLASSES DE HEROI ----\n|     1 - Guerreiro      |\n|     2 - Mago           |\n|     3 - Monge          |\n|     4 - Ninja          |\n--------------------------\nEscolha a classe do seu heroi: "))
 
     while (classeHeroi < 1 || classeHeroi > 4) {
         classeHeroi = parseInt(readline.question("\nOpcao incorreta, tente novamente\n\n---- CLASSES DE HEROI ----\n|     1 - Guerreiro      |\n|     2 - Mago           |\n|     3 - Monge          |\n|     4 - Ninja          |\n--------------------------\nEscolha a classe do seu heroi: "))
     }
 
+    return classeHeroi
+}
+
+function tipoHeroi(tipos){
     switch (classeHeroi) {
         case 1:
             tipo = "Guerreiro"
@@ -37,6 +42,7 @@ do {
     let idade = readline.question("Qual a idade do seu Heroi? ")
 
     classe()
+    tipoHeroi(classeHeroi)
     console.log(`\nO ${tipo} atacou usando ${ataque}\n`)
 
     var repet = readline.question("Nova classificacao? S/N\n")
